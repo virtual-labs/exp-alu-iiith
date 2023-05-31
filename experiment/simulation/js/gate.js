72edfa65d1a54e38acd8073fb14aade4fad2b570
Ananya Vaibhavi Pabba <anivaibh24@gmail.com>
2023-05-31 23:25:48 +0530

    Resfresh i/o bits after submit (#21)

--- experiment/simulation/js/gate.js
+++ experiment/simulation/js/gate.js
@@ -214,6 +214,24 @@ window.setInput = setInput;
 export function submitCircuit() {
     clearResult();
     document.getElementById("table-body").innerHTML = "";
+
+    // Refresh the input bit values to default 1 and output bit values to default empty black circles after submitting
+    for (let gateId in gates) {
+        const gate = gates[gateId];
+        if (gate.isInput) {
+            gate.setOutput(true);
+            let element = document.getElementById(gate.id);
+            element.className = "high";
+            element.childNodes[0].innerHTML = "1";
+        }
+        if(gate.isOutput) {
+            gate.setOutput(null);
+            let element = document.getElementById(gate.id);
+            element.className = "output";
+            element.childNodes[0].innerHTML = "";
+        }
+    }
+
     if (window.currentTab === "task1") {
         validateAlu("Input-0", "Input-1", "Input-2","Input-3","Input-4", "Output-5", "Output-6");
     }
